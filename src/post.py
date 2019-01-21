070f85b75e67c96a7f9e036d7204ac4877aa07a1
AboutCode <therealaboutcode@gmail.com>
2019-01-20 16:14:28 -0800

    Third commit

---
+++ src/post.py
@@ -0,0 +1,9 @@
+# THE CODE OF ABOUTCODE:
+  # initialize empty List
+  # your reading list, right up until now, was ...
+  myReadingList = []
+  # .. empty
+
+  # now, your new reading list has five different new items.
+  myNewReadingList = ["Link 1", "Link 2", "Discussion 1", "Discussion 2", "Disc\
+ussion 3"]
